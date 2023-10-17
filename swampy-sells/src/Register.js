0edf4c79e91683119bc581544802ec104848bbc3
Danielle Silfa <dnsilfa@gmail.com>
2023-10-17 17:37:05 -0400

    updated login page

--- swampy-sells/src/Register.js
+++ swampy-sells/src/Register.js
@@ -4,7 +4,9 @@ import React, { useState } from "react";
 export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
 
     const submitForm = (e) => {
         //e.preventDefault is to prevent page from being reloaded
@@ -17,15 +19,17 @@ export const Register = (props) => {
             <img className="register-logo"src="../swampysells-logo.png"></img>
             <h1>Create Account</h1>
             <form className="register-form" onSubmit={submitForm}>
-                <label htmlFor="name">Full name </label>
-                <input value={name} name="name" id="name" placeholder="Full name"/>
-                <label htmlFor="email">email </label>
+                <label htmlFor="firstName">First name</label>
+                <input value={firstName} onChange={(e) => setFirstName(e.target.value)} name="firstName" id="firstName" placeholder="First name"/>
+                <label htmlFor="lastName">Last name</label>
+                <input value={lastName} onChange={(e) => setLastName(e.target.value)} name="lastName" id="lastName" placeholder="Last name"/>
+                <label htmlFor="email">Email </label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email address" id="email" name="email" />
-                <label htmlFor="password">password </label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <label htmlFor="password">Password </label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter password" id="password" name="password" />
                 <button type="submit">Create Account</button>
             </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Log in here.</button>
         </div>
     )
 
