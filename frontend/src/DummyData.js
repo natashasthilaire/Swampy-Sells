8134d4c2ac033c977db7d384167bffe7f47e3603
bllndalichako <bndalichako@ufl.edu>
2023-11-26 19:00:41 -0500

    Implemented profile page frontend

---
+++ frontend/src/DummyData.js
@@ -0,0 +1,113 @@
+export const bookmarks = [
+  {
+    id: 1,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 8,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 9,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  }
+]
+
+export const posts = [
+  {
+    id: 1,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 8,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 9,
+    title: "Macbook Pro 2019",
+    price: "$1000",
+    image: "https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  }
+]
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,9 +1,104 @@
 import Header from "./Header";
 import React from "react";
-import { NavBar } from "./NavBar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { bookmarks, posts } from "../DummyData"
+import "../styles/Profile.css";
 
-export const Profile = () => {
-    return <div>
-        <Header />
+// TODO(bllndalichako): Replace dummy data with real data
+export const Profile = (props) => {
+  const [user, setUser] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5555/users/${id}`)
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return <div>
+    {/* TODO(bndalichako): Remove dummy data */}
+    <Header />
+    <div className="profile">
+      <div className="view">
+        <div className="info">
+          <div className="top-info">
+            <div className="profile-info">
+              <img className="profile-img"
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&dpr=2"
+                alt=""
+              />
+              <div className="identifiers">
+                <h1 className="student-name">Gator Student</h1>
+                {/*<h1>{user.firstName} {user.lastName}</h1>*/}
+                <div className="activity">
+                  {/*TODO(bndalichako): Remove constants*/}
+                  <p>{user.postsCount} 23 items listed</p>
+                  <p>{user.salesCount} 5 items sold</p>
+                  <p>{user.purchasesCount} 9 items purchased</p>
+                </div>
+              </div>
+            </div>
+            <div className="profile-links">
+              {/*TODO(bndalichako): Add logout functionality */}
+              <button className="logout-button">Log Out</button>
+            </div>
+          </div>
+          <div className="bottom-info">
+            <div className="headings">
+              <h1 className="posts-heading">Posts</h1>
+              <h1 className="bookmarks-heading">Bookmarks</h1>
+            </div>
+            <div className="posts">
+              <div className="posts-list">
+                {/*{user.bookmarks?.map((post) => (*/}
+                {posts?.map((post) => (
+                  <div className="postItem" key={post.id}>
+                    <div className="post">
+                      <img className="post-img"
+                        src={post.image}
+                        alt=""
+                      />
+                      <div className="post-info">
+                        <p className="post-title">{post.title}</p>
+                        <p className="post-price">{post.price}</p>
+                      </div>
+
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="bookmarks">
+              <div className="bookmarks-list">
+                {/*{user.bookmarks?.map((post) => (*/}
+                {bookmarks?.map((post) => (
+                  <div className="bookmark" key={post.id}>
+                    <div className="post">
+                      <img className="post-img"
+                        src={post.image}
+                        alt=""
+                      />
+                      <div className="post-info">
+                        <p className="post-title">{post.title}</p>
+                        <p className="post-price">{post.price}</p>
+                      </div>
+
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 }
