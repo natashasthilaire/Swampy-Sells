0f46a87b1f52cdce91048e1d6509f31b717a79b9
rrosalinda <101357257+rrosalinda@users.noreply.github.com>
2023-11-28 22:55:12 -0500

    Revert "updated messing ui"
    This reverts commit 1c20f466d071eb319e63eece4f7f492b40ed86bf.

---
+++ frontend/src/components/Inbox.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import React from "react";
+import { NavBar } from "./NavBar";
+
+
+export const Inbox = () => {
+    return <div className="app">
+        <Header />
+        <h2>Messaging Page</h2>
+    </div>
+}
