7c366997046922c3ba5a5dd5f619810bb740e08e
Natasha St Hilaire <natashasthilaire@ufl.edu>
2023-11-29 00:42:45 -0500

    dependencies & erased alert function:

--- frontend/src/components/ItemDetails.js
+++ frontend/src/components/ItemDetails.js
@@ -7,7 +7,7 @@ const ItemDetails = ({item}) => {
 
   
   return (
-    <div className='item-details' onClick={() => alert('hello')}>
+    <div className='item-details'>
       <img className='bookmark' src={bookmark} alt=""/>
       <img className="home-img" style={{marginTop: "15px"}}src={`data:image/jpeg;base64,${Buffer.from(item.image).toString('base64')}`}></img>
       <h5 style={{marginTop:"10px", width:"200px"}}>{item.title}</h5>
