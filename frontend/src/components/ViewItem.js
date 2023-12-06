7bec433854cec075349d355749d033275fee894a
Natasha St Hilaire <natashasthilaire@ufl.edu>
2023-12-06 01:25:49 -0500

    view item styling changes

--- frontend/src/components/ViewItem.js
+++ frontend/src/components/ViewItem.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
+import { useAuth } from '../context/AuthProvider';
 import { useNavigate, useParams } from 'react-router-dom';
 import Header from "./Header";
 import { Buffer } from 'buffer';
@@ -9,6 +10,7 @@ import '../styles/ViewItem.css';
 export const ViewItem = () => {
     const [details, setDetails] = useState({});
     const { id } = useParams();
+    const user = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -24,13 +26,11 @@ export const ViewItem = () => {
                     description: responseData.description,
                     image: `data:image/png;base64, ${base64}`,
                     condition: responseData.condition,
-                    sellerId: responseData.user
+                    sellerId: responseData.user,
+                    sellerName: responseData.user.location
                     // comments: responseData.comments
+                    
                 });
-                console.log(responseData.image)
-                console.log(responseData.image.data);
-
-
 
         } catch (error) {
             console.error('Error with fetching in useEffect', error);
@@ -39,16 +39,8 @@ export const ViewItem = () => {
     retrieveItem();
 }, [id]);
 
-    // const sleep = (time) => {
-    //     return new Promise(resolve => setTimeout(resolve, time))
-    // }
-
     const handleMessageButton = async() => {
-        console.log(details.sellerId);
-        // await sleep(2000);
-        //itemDetails({ sellerId: details.sellerId})
         navigate('/inbox');
-
     }
 
 
@@ -60,26 +52,27 @@ return (
         <div className= "item-image-container">
         <img src={details.image} alt="ItemImage" />
         </div>
+        <h2>{details.sellerName}</h2>
         <div className="item-details">
             <div className="details">
-                
                 <h2 className='item-title'>{details.title}</h2>
                 <p className='item-price'>${details.price}</p>
                 <p className='item-condition'>Condition: {details.condition}</p>
                 <div className='item-description-container'>
+                <div style={{display:"flex"}}>
                 <p className='item-description'>Description:  </p>
-                <div className='item-description-details'>{ details.description}</div>  
+                <div className='item-description-details'>{details.description}</div>  
+                </div>
                 </div>
                 <Button variant="outline-success" 
-                    style={{backgroundColor:"#0000FF", color:"white", borderColor:"#0000FF"}}
+                    style={{backgroundColor:"#0000FF", color:"white", borderColor:"#0000FF", width:"150px", height:"58px", fontSize:"30px", justifyContent:"center"}}
                     onClick={handleMessageButton}>
                     Message
                 </Button>
 
             </div>
         </div>
-        {/* <Button variant="outline-success" style={{backgroundColor:"#0000FF", color:"white", borderColor:"#0000FF"}}>Search</Button> */}
-    </div>
+        </div>
     </div>
 
     
